8d25361d482aa13bcb477669bd1dde094d0b19b9
cart0uche <hafid.saou@gmail.com>
2013-08-11 01:37:07 +0200

    POC

---
+++ pySMS_Android.py
@@ -0,0 +1,27 @@
+import android
+
+from bottle import run, post, error, request, get
+
+@post('/send_sms')
+def send_sms():
+	number  = request.forms.get('number')
+	message = request.forms.get('message')
+	print number + ' : ' + message
+	droid.smsSend(number,message)
+
+@get('/send_sms')
+def fill_sms():
+	return '''
+	 <form action="/send_sms" method="post">
+			Number : <input name="number" type="text" />
+			Message: <input name="message" type="text" />
+			<input value="Send" type="submit" />
+		</form>
+	'''
+
+@error(404)
+def error404(error):
+	return 'Nothing here sorry'
+
+droid = android.Android()
+run(host='192.168.1.41', port=80, reloader=True)
